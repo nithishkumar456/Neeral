f4eb9a031ce56594882ca69d1f21b68786de7cf9
Nithish Kumar <57395492+nithishkumar456@users.noreply.github.com>
2025-08-28 19:10:23 +0200

    adds new input system

--- Neeral/Assets/Scripts/CameraScript/CameraFollow.cs
+++ Neeral/Assets/Scripts/CameraScript/CameraFollow.cs
@@ -1,20 +1,20 @@
-using UnityEngine;
+    using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-
-    public Transform playerTransform;
-
-    private Vector3 offset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CameraFollow : MonoBehaviour
     {
-        offset = transform.position - playerTransform.position;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = playerTransform.position + offset;
+        public Transform playerTransform;
+
+        private Vector3 offset;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            offset = transform.position - playerTransform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = playerTransform.position + offset;
+        }
     }
-}
