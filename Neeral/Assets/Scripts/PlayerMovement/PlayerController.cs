c8425dfed223da01de8b7d64843f82146e3c8a02
Nithish Kumar <57395492+nithishkumar456@users.noreply.github.com>
2025-08-28 18:59:58 +0200

    adds basic player movement control

---
+++ Neeral/Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements.Experimental;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private CharacterController controller;
+
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDir = new Vector3(-moveInput.x, 0, -moveInput.y);
+        if (moveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDir);
+            controller.Move(moveDir * moveSpeed * Time.deltaTime);
+        }
+    }
+ 
+}
